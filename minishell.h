bb5d53db0711b4002faee70e799880e294ab6069
ppaula-d <ppaula-d@student.42.porto.com>
2025-04-15 08:59:53 +0100

    updated .h file, parsing notes and added files to the srcs

--- minishell.h
+++ minishell.h
@@ -2,4 +2,23 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define MAX_TOKENS 100
+# define MAX_TOKEN_LENGTH 256
+
+typedef enum s_tokens
+{
+    COMMAND,
+    ARGUMENT,
+    OPERATOR,
+    DELIMETER,
+    QUOTE,
+    COMMENT
+}   t_tokens;
+
+typedef struct s_token
+{
+    t_tokens type; // token type
+    char *value; // token value
+}   t_token;
+
 #endif
