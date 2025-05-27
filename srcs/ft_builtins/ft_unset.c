704daa8d2de87bb217d5732978328571ad0acd55
Andre Filipe Barbosa Miranda <afilipe-@c1r11s2.42porto.com>
2025-05-27 16:25:42 +0100

    unset start

---
+++ srcs/ft_builtins/ft_unset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afilipe- <afilipe-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 16:04:50 by afilipe-          #+#    #+#             */
+/*   Updated: 2025/05/27 16:25:08 by afilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int ft_unset(t_shell *shell, char **args)
+{
+
+}
+
+int validate_unset_args(const char *args)
+{
+
+}
+
+void env_remove(t_env **head, const char *key)
+{
+	
+}
