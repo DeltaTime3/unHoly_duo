d4f19be9f994bb53faa6889ffdf764f8051792e3
Andre Filipe Barbosa Miranda <afilipe-@c1r11s2.42porto.com>
2025-06-12 13:41:49 +0100

    executer, please please work, yuo bastard

--- srcs/exect.c
+++ srcs/exect.c
@@ -6,7 +6,7 @@
 /*   By: afilipe- <afilipe-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 11:07:06 by afilipe-          #+#    #+#             */
-/*   Updated: 2025/06/11 15:31:51 by afilipe-         ###   ########.fr       */
+/*   Updated: 2025/06/12 13:39:24 by afilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,12 +60,11 @@ int	ft_execute(t_shell *shell, t_token *value)
 	if (is_builtin(value))
 	{
 		choose_b_in(value, shell);
-		return (0);
+		return (shell->exit_code);
 	}
 	else
 	{
-		execute2(shell, value);
-		return (0);
+		return (execute2(shell, value));
 	}
 }
 //here I have to use child processes;
@@ -96,6 +95,8 @@ int	execute2(t_shell *shell, t_token *token)
 	pid = fork();
 	if (pid == 0)
 	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
 		execve(full_path, token->args, env_array);
 		perror("execve");
 		free(full_path);
@@ -122,14 +123,103 @@ int	execute2(t_shell *shell, t_token *token)
 char	*get_cmd_path(char *cmd, t_env *env)
 {
 	char		*path;
-	char		*dir;
+	char		*pth_cpy;
+	char		*res;
 	char		*full_path;
-	struct stat	st;
 
 	if (!cmd || !*cmd)
 		return (NULL);
 	if (ft_strchr(cmd, '/'))
 	{
-		
+		if (access(cmd, X_OK) == 0)
+			return ft_strdup(cmd);
+		return (NULL);
 	}
+	path = get_env_value(env, "PATH");
+	if (!path || path[0] == '\0')
+	{
+		if (pth_cpy)
+			free(pth_cpy);
+		return (NULL);
+	}		
+	pth_cpy = ft_strdup(path);
+	if (!pth_cpy)
+		return (NULL);
+	res = check_dir(pth_cpy, cmd);
+	return(free(pth_cpy), res);	
+}
+
+char	*check_dir(char *pth_cpy, char *cmd)
+{
+	char	*dir;
+	char	*full_path;
+	char	*res;
+	
+	res = NULL;
+	while (dir = get_next_path(&pth_cpy))
+	{
+		full_path = build_cmb_path(dir, cmd);
+		free(dir);
+		if (full_path && access(full_path, X_OK) == 0)
+		{
+			res = full_path;
+			break;
+		}
+		free(full_path);
+	}
+	return (res);
+}
+
+char	*get_next_path(char **path_temp)
+{
+	char	*start;
+	char	*end;
+	char	*dir;
+	int		len;
+
+	start = *path_temp;
+	if(!start || !*start)
+		return (NULL);
+	end = ft_strchr(start, ':');
+	if (end != NULL)
+		len = end - start;
+	else
+		len = ft_strlen(start);
+	dir = malloc(len + 1);
+	if (!dir)
+		return (NULL);
+	ft_strlcpy(dir, start, len + 1);
+	dir[len] = '\0';
+	if (end)
+		*path_temp = end + 1;
+	else
+		*path_temp = start + len;
+	return (dir);
+}
+
+char	*build_cmb_path(char *dir, char *cmd)
+{
+	char	*full_path;
+	size_t	dir_len;
+	size_t	cmd_len;
+	size_t	total_len;
+
+	dir_len = ft_strlen(dir);
+	cmd_len = ft_strlen(cmd);
+	if (!dir[0])
+		total_len = cmd_len + 3;
+	else
+		total_len = dir_len + cmd_len + 2;
+	full_path = malloc(total_len);
+	if(!full_path)
+		return (NULL);
+	if(!dir[0])
+		ft_strlcpy(full_path, "./", 3);
+	else
+	{
+		ft_strlcpy(full_path, dir, total_len);
+		ft_strlcat(full_path, "/", total_len);
+	}
+	ft_strlcat(full_path, cmd, total_len);
+	return(full_path);	
 }
