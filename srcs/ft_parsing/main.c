aaa172ca918c1037f14931c5e47f019839a26e9f
Andre Filipe Barbosa Miranda <afilipe-@c1r11s2.42porto.com>
2025-06-17 15:03:33 +0100

    PATH working

--- srcs/ft_parsing/main.c
+++ srcs/ft_parsing/main.c
@@ -7,6 +7,8 @@ int	main(int ac, char **av, char **envp)
     t_token	*tokens;
     t_shell	shell;
     int     is_valid;
+	char	*expanded;
+	char	*full_path;
 
     (void)ac;
     (void)av;
@@ -36,8 +38,31 @@ int	main(int ac, char **av, char **envp)
                 free(input);
                 if (tokens)
                 {
-                    ft_execute(&shell, tokens);
-                    free_tokens(tokens);
+                	if (is_builtin(tokens))
+					{
+						ft_execute(&shell, tokens);
+					}
+					else if (is_sp_expantion(tokens))
+					{
+						expanded = expand_token_value(tokens->value, &shell);
+						printf("%s\n", expanded);
+						free(expanded);
+					}
+					else
+					{
+						full_path = get_cmd_path(tokens->value, shell.head);
+						if (full_path)
+						{
+							ft_execute(&shell, tokens);
+							free(full_path);
+						}
+						else
+						{
+							ft_printf_fd(2, "minishell: %s: command not found\n", tokens->value);
+							shell.exit_code = 127;
+						}                   
+					}
+					free_tokens(tokens);
                 }
             }
             else
@@ -47,4 +72,35 @@ int	main(int ac, char **av, char **envp)
     rl_clear_history();
     free_env(shell.head);
     return (shell.exit_code);
+}
+
+int	is_sp_expantion(t_token *token)
+{
+	const char	*value;
+	const char	*end;
+	int			i;
+
+	value = token->value;
+	if (!value || !*value)
+		return (0);
+	if (value[0] == '~' && (value[1] == '\0' || value[1] == '/'))
+		return (1);
+	if (value[0] == '$')
+	{
+		if (value[1] == '{')
+		{
+			end = strchr(value + 2, '}');
+			if (end && end[1] == '\0')
+				return (1);
+		}
+		else if (ft_isalpha(value[1]) || value[1] == '_')
+		{
+			i = 2;
+			while (value[i] && (ft_isalnum(value[i]) || value[i] == '_'))
+				i++;
+			if(value[i] == '\0')
+				return(1);
+		}
+	}
+	return (0);
 }
